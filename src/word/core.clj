b3f7c352fe925bd60b7c075de7a2867d0a5c52c3
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-28 16:39:20 +0100

    refactor - remove recursive dependency between 'wrap' and 'break-between': extract break-between private function

--- src/word/core.clj
+++ src/word/core.clj
@@ -7,23 +7,23 @@
 (defn- last-whitespace-in [phrase columns]
   (string/last-index-of phrase " " columns))
 
+(defn- break-between [phrase start end]
+  (list
+    (concat (subs phrase 0 start) "\n")
+    (subs phrase end)))
+
 (defn wrap [phrase columns]
   (loop [phrase phrase
          columns columns
          wrapped-phrase nil]
-    (let [break-between
-          (fn [phrase start end]
-            (list
-              (concat (subs phrase 0 start) "\n")
-              (subs phrase end)))]
-      (if (fits-in? phrase columns)
-        (string/join (concat wrapped-phrase phrase))
-        (if-let [whitespace-pos (last-whitespace-in phrase columns)]
-          (recur
-            (second (break-between phrase whitespace-pos (inc whitespace-pos)))
-            columns
-            (concat wrapped-phrase (first (break-between phrase whitespace-pos (inc whitespace-pos)))))
-          (recur
-            (second (break-between phrase columns columns))
-            columns
-            (concat wrapped-phrase (first (break-between phrase columns columns)))))))))
+    (if (fits-in? phrase columns)
+      (string/join (concat wrapped-phrase phrase))
+      (if-let [whitespace-pos (last-whitespace-in phrase columns)]
+        (recur
+          (second (break-between phrase whitespace-pos (inc whitespace-pos)))
+          columns
+          (concat wrapped-phrase (first (break-between phrase whitespace-pos (inc whitespace-pos)))))
+        (recur
+          (second (break-between phrase columns columns))
+          columns
+          (concat wrapped-phrase (first (break-between phrase columns columns))))))))
