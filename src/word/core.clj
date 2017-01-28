bfcbfea03729e9fd6e605063f4c5bf976c0dddb7
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-28 13:19:43 +0100

    refactor - remove recursive dependency between 'wrap' and 'break-between': introduce loop

--- src/word/core.clj
+++ src/word/core.clj
@@ -8,14 +8,17 @@
   (string/last-index-of phrase " " columns))
 
 (defn wrap [phrase columns]
-  (let [break-between
-        (fn [phrase start end]
-          (string/join
-            (cons (subs phrase 0 start)
-                  (cons "\n"
-                        (wrap (subs phrase end) columns)))))]
-    (if (fits-in? phrase columns)
-      phrase
-      (if-let [whitespace-pos (last-whitespace-in phrase columns)]
-        (break-between phrase whitespace-pos (inc whitespace-pos))
-        (break-between phrase columns columns)))))
+  (loop [phrase phrase
+         columns columns
+         wrapped-phrase nil]
+    (let [break-between
+          (fn [phrase start end]
+            (string/join
+              (cons (subs phrase 0 start)
+                    (cons "\n"
+                          (wrap (subs phrase end) columns)))))]
+      (if (fits-in? phrase columns)
+        phrase
+        (if-let [whitespace-pos (last-whitespace-in phrase columns)]
+          (break-between phrase whitespace-pos (inc whitespace-pos))
+          (break-between phrase columns columns))))))
