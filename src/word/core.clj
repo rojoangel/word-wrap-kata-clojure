f6ef04574514954661d831e6350abb3b2626f269
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-28 13:01:21 +0100

    refactor - extract to 'fits-in?' function to improve expressiveness

--- src/word/core.clj
+++ src/word/core.clj
@@ -1,6 +1,9 @@
 (ns word.core
   (:require [clojure.string :as string]))
 
+(defn- fits-in? [phrase columns]
+  (<= (count phrase) columns))
+
 (defn wrap [phrase columns]
   (let [break-between
         (fn [phrase start end]
@@ -8,7 +11,7 @@
             (cons (subs phrase 0 start)
                   (cons "\n"
                         (wrap (subs phrase end) columns)))))]
-    (if (<= (count phrase) columns)
+    (if (fits-in? phrase columns)
       phrase
       (if-let [whitespace-pos (string/last-index-of phrase " " columns)]
         (break-between phrase whitespace-pos (inc whitespace-pos))
