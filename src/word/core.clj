d5a22180db986f616f57ae4bb730bff8389f6c91
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-28 13:05:05 +0100

    refactor - extrat to 'last-whitespace-in' function to improve expressiveness

--- src/word/core.clj
+++ src/word/core.clj
@@ -4,6 +4,9 @@
 (defn- fits-in? [phrase columns]
   (<= (count phrase) columns))
 
+(defn- last-whitespace-in [phrase columns]
+  (string/last-index-of phrase " " columns))
+
 (defn wrap [phrase columns]
   (let [break-between
         (fn [phrase start end]
@@ -13,6 +16,6 @@
                         (wrap (subs phrase end) columns)))))]
     (if (fits-in? phrase columns)
       phrase
-      (if-let [whitespace-pos (string/last-index-of phrase " " columns)]
+      (if-let [whitespace-pos (last-whitespace-in phrase columns)]
         (break-between phrase whitespace-pos (inc whitespace-pos))
         (break-between phrase columns columns)))))
