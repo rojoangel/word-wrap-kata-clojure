a117e10a17fba2fc68f2a41f90559a718e044b3f
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-28 13:00:13 +0100

    bump midje version up to latest stable in order to get rid of several warnings

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject word "0.0.1-SNAPSHOT"
   :description "word wrap kata"
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]}})
   
